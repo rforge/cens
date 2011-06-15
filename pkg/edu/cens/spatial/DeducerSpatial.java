48fd3228e67785282cb7445c917cdc98a9b94103
ifellows <ifellows@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-15 04:49:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cens@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/edu/cens/spatial/DeducerSpatial.java
@@ -0,0 +1,81 @@
+package edu.cens.spatial;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import org.rosuda.JGR.JGR;
+import org.rosuda.deducer.data.DataViewerController;
+import org.rosuda.ibase.toolkit.EzMenuSwing;
+
+
+public class DeducerSpatial {
+	
+	public static ActionListener cListener = new SpatialMenuListener();
+	
+	public static void init(){
+		try{
+		int menuIndex=6;
+		
+		insertMenu(JGR.MAINRCONSOLE,"Spatial",menuIndex);
+		EzMenuSwing.addJMenuItem(JGR.MAINRCONSOLE, "Spatial", "Load shape file", "shape", cListener);
+		EzMenuSwing.addJMenuItem(JGR.MAINRCONSOLE, "Spatial", "Convert data.frame", "conv_pnt", cListener);
+		DataViewerController.init();
+		DataViewerController.addDataType("SpatialPointsDataFrame", "sp-p");
+		DataViewerController.addTabFactory("SpatialPointsDataFrame","Data View", new SpatialDataViewFactory());
+		DataViewerController.addTabFactory("SpatialPointsDataFrame","Variable View", new SpatialVariableViewFactory());
+		DataViewerController.addTabFactory("SpatialPointsDataFrame","Coordinates", new CoordViewFactory(false));
+
+		DataViewerController.addDataType("SpatialPolygonsDataFrame", "sp-py");
+		DataViewerController.addTabFactory("SpatialPolygonsDataFrame","Data View", new SpatialDataViewFactory());
+		DataViewerController.addTabFactory("SpatialPolygonsDataFrame","Variable View", new SpatialVariableViewFactory());
+		DataViewerController.addTabFactory("SpatialPolygonsDataFrame","Centroids", new CoordViewFactory(false));
+		
+		DataViewerController.addDataType("SpatialLinesDataFrame", "sp-l");
+		DataViewerController.addTabFactory("SpatialLinesDataFrame","Data View", new SpatialDataViewFactory());
+		DataViewerController.addTabFactory("SpatialLinesDataFrame","Variable View", new SpatialVariableViewFactory());
+		DataViewerController.addTabFactory("SpatialLinesDataFrame","Paths", new CoordViewFactory(true));
+		
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void insertMenu(JFrame f, String name,int index) {
+		JMenuBar mb = f.getJMenuBar();
+		JMenu m = EzMenuSwing.getMenu(f,name);
+		if (m == null && index<mb.getMenuCount()){
+			JMenuBar mb2 = new JMenuBar(); 
+			int cnt = mb.getMenuCount();
+			for(int i=0;i<cnt;i++){
+				if(i==index)
+					mb2.add(new JMenu(name));
+				mb2.add(mb.getMenu(0));
+			}
+			f.setJMenuBar(mb2);			
+		}else if(m==null && index==mb.getMenuCount())
+			EzMenuSwing.addMenu(f,name);
+	}
+}
+
+class SpatialMenuListener implements ActionListener{
+
+	public void actionPerformed(ActionEvent act) {
+		String cmd = act.getActionCommand();
+		if(cmd.equals("shape")){
+			ShapeFileLoader sfl = new ShapeFileLoader();
+			sfl.setVisible(true);
+			sfl.runInR();
+		}else if(cmd.equals("conv_pnt")){
+			DFPointConvertDialog conv= DFPointConvertDialog.getInstance();;
+			conv.run();
+		}
+	}
+	
+}
+
---
+++ pkg/edu/cens/spatial/ShapeFileLoader.java
@@ -0,0 +1,59 @@
+package edu.cens.spatial;
+
+import java.awt.FlowLayout;
+import java.awt.Frame;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.rosuda.JGR.RController;
+import org.rosuda.JGR.toolkit.FileSelector;
+import org.rosuda.deducer.Deducer;
+
+public class ShapeFileLoader extends FileSelector{
+	
+	private JTextField rDataNameField;
+	JTextField proj ;
+	public ShapeFileLoader(Frame f) {
+		super(f, "Load shape file", FileSelector.LOAD, null, true);
+		JPanel namePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		namePanel.add(new JLabel("Set name: "));
+		rDataNameField = new JTextField(12);
+		namePanel.add(rDataNameField);
+		namePanel.add(new JLabel("      Proj: "));
+
+		proj = new JTextField(10);
+		proj.setText("+proj=longlat");
+		namePanel.add(proj);
+		this.addFooterPanel(namePanel);
+	}
+	
+	public ShapeFileLoader() {
+		this(null);
+	}
+	
+	public boolean runInR(){
+		if (this.getFile() == null)
+			return false;
+		String rName = rDataNameField.getText();
+		if (rName.length() == 0)
+			rName = (this.getFile().indexOf(".") <= 0 ? Deducer.getUniqueName(this.getFile()) : 
+					Deducer.getUniqueName(this.getFile().substring(0, this.getFile().indexOf("."))));
+		rName = RController.makeValidVariableName(rName);
+		String path =(this.getDirectory() + Deducer.addSlashes(this.getFile())).replace('\\', '/');
+		if(!path.toLowerCase().endsWith(".shp") &&
+				!path.toLowerCase().endsWith(".shp") &&
+				!path.toLowerCase().endsWith(".dbf") &&
+				!path.toLowerCase().endsWith(".prj") &&
+				!path.toLowerCase().endsWith(".sbn") &&
+				!path.toLowerCase().endsWith(".sbx")){
+			JOptionPane.showMessageDialog(this, "This does not appear to be a shape file.\nAcceptable extensions are: .shp,.dbf,.prj,.sbx,.sbn");
+			return false;
+		}
+		Deducer.execute(rName +" <- readShapeSpatial(\""+path+"\", proj=CRS('"+proj.getText()+"'))");
+		return true;
+	}
+	
+}
