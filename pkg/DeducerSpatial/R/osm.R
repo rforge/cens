15266fa9f909b9ab12f64451e0415ce35401fce4
ifellows <ifellows@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-22 17:34:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cens@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/DeducerSpatial/R/osm.R
@@ -0,0 +1,82 @@
+# TODO: Add comment
+# 
+# Author: ianfellows
+###############################################################################
+
+
+
+#'get an open street map tile. tpe can be "osm" or "bing"
+osmtile <- function(x,y,zoom,type="osm"){
+	x <- as.double(x)
+	y <- as.double(y)
+	zoom <- as.double(zoom)
+	TC <- J("edu.cens.spatial.RTileController")
+	res <- TC$getInstance(type)$getTileValues(x,y,zoom)
+	res <- as.character(as.hexmode(res))
+	red <- substr(res, 3, 4)
+	green <- substr(res, 5, 6)
+	blue <- substr(res, 7, 8)
+	colrs <- paste("#", red, green, blue, sep = "")
+	
+	sc <- 20037508*2
+	minim <- -20037508
+	
+	p1 <- c(x/(2^zoom)*sc+minim,-(y/(2^zoom)*sc+minim))
+	p2 <- c((x+1)/(2^zoom)*sc+minim,-((y+1)/(2^zoom)*sc+minim))
+	bbox <- list(p1=p1,p2=p2)
+	res <- list(colorData=colrs,bbox=bbox)
+	class(res) <- "osmtile"
+	res
+}
+
+#'add tile to plot
+plot.osmtile <- function(x,y=NULL,add=TRUE,...){
+	image(x=seq(x$bbox$p1[1],x$bbox$p2[1],length=255),
+			y=seq(x$bbox$p2[2],x$bbox$p1[2],length=255),
+			z=matrix(1:(255*255),ncol=255)[,255:1],
+			col=x$colorData,add=add)
+	
+}
+
+#' get a map based on lat long coordinates 
+openmap <- function(upperLeft,lowerRight,zoom,type="osm"){
+	zoom <- as.integer(zoom)
+	ts <- new(J("org.openstreetmap.gui.jmapviewer.tilesources.BingAerialTileSource"))
+	minY <-as.integer(floor(ts$latToTileY(upperLeft[1],zoom)))
+	maxY <-as.integer(floor(ts$latToTileY(lowerRight[1],zoom)))
+	
+	minX <-as.integer(floor(ts$lonToTileX(upperLeft[2],zoom)))
+	maxX <-as.integer(floor(ts$lonToTileX(lowerRight[2],zoom)))
+	
+	map <- list(tiles=list())
+	for( x in minX:maxX){
+		for(y in minY:maxY){
+			tile <- osmtile(x,y,zoom,type)
+			map$tiles[[length(map$tiles)+1]] <- tile
+		}
+	}
+	map$bbox <- list(p1=project_mercator(upperLeft[1],upperLeft[2]),p2=project_mercator(lowerRight[1],lowerRight[2]))
+	class(map) <- "OpenStreetMap"
+	map
+}
+
+#'plot the map in mercator coordinates. see osm().
+plot.OpenStreetMap <- function(x,y=NULL,add=FALSE,...){
+	if(add==FALSE){
+		plot.new()
+		par(mar=c(0,0,0,0))
+		plot.window(xlim=c(x$bbox$p1[1],x$bbox$p2[1]),ylim=c(x$bbox$p2[2],x$bbox$p1[2]), 
+				xaxs = 'i', yaxs = 'i',asp=abs((x$bbox$p2[2]-x$bbox$p1[2])/(x$bbox$p1[1]-x$bbox$p2[1])))
+	}
+	for(tile in x$tiles)
+		plot(tile)
+}
+
+#m <- c(25.7738889,-80.1938889)
+#j <- c(58.3019444,-134.4197222)
+#miami <- project_mercator(25.7738889,-80.1938889)
+#jun <- project_mercator(58.3019444,-134.4197222)
+#data(states)
+#map <- openmap(j,m,4)
+#plot(map)
+#plot(states,add=T)
