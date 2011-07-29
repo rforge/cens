fa2f273df7ea477f48798315ba0b04528109f049
alexrickett <alexrickett@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-29 00:34:21 +0000

    Removed unused functions from text.R, updated DESCRIPTION FILE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cens@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerText/R/text.R
+++ pkg/DeducerText/R/text.R
@@ -1,12 +1,36 @@
+# Author: ianfellows
+###############################################################################
+
+#d: the corpus for which term frequencies are calculated
+#
+#topN: If specified, only the 'topN' most frequent terms are returned. If more 
+#      terms are requested than available, all terms are returned.
+#
+#percent: If specified, only the top 'percent' % of terms are returned. If more
+#	  terms are requested than available, all terms are returned.
+#
+#sorted: A string specifying how to sort the terms.  'none' for no sorting,
+#	'alpha' for alphanumeric sorting, and 'freq' for sorting by frequency.
+#
+#decreasing: If TRUE, terms are sorted in decreasing order, if FALSE, sorted
+#	     ascending order.
+#
+#useDocFreq: If TRUE, the returned frequencies are for the total number of
+#	documents in which the term occurs.  If false, they are the total 
+#	number of occurrences.
+#
+#minFreq: Terms with *TOTAL* frequencies below this threshold will not be 
+#	  included in the output.
+#
 
 cens.term_freq <- function(
 		d, 
-		percent=0,
 		topN=0, #only include the first 'topN' terms
+		percent=0,
 		sorted=c("none", "alpha", "freq"),
-		minFreq=1,
-        decreasing=FALSE,
-		useDocFreq=FALSE) {
+		decreasing=FALSE,
+		useDocFreq=FALSE,		
+		minFreq=1) {
 	
 	dtm <- DocumentTermMatrix(d, control = list(tolower=FALSE, minWordLength=1));
 	
@@ -22,13 +46,13 @@ cens.term_freq <- function(
 
   sorted <- match.arg(sorted);
 
-  if(percent > 0) { #use percentage of terms
-    o <- order(x, decreasing=TRUE)[1:(length(x) * percent / 100)];
-    x <- x[o];
-  } else if (topN > 0) { #use absolute number of terms
+  if (topN > 0) { #use absolute number of terms
 	  o <- order(x, decreasing=TRUE)[1:min(topN, length(x))];
 	  x <- x[o];
-  }
+  } else if(percent > 0) { #use percentage of terms
+    o <- order(x, decreasing=TRUE)[1:(length(x) * percent / 100)];
+    x <- x[o];
+  } 
 
   if(sorted == "alpha") {
     x <- x[order(names(x), decreasing=decreasing)];
@@ -38,12 +62,6 @@ cens.term_freq <- function(
   }
 
   return(x);
-}
-
-cens.getCorpusNames <- function() 
-{
-	get.objects("Corpus")
-  #names(which(unlist(eapply(.GlobalEnv, function(n) "Corpus" %in% class(n)))));
 }
 
 make.color.scale<- function(aColor, bColor, steps, gradientExp=.5){
@@ -57,29 +75,5 @@ make.color.scale<- function(aColor, bColor, steps, gradientExp=.5){
 	}
 	return(ret);
 }
-
-cens.choose_corpus <- function(){
-  require(rJava);
-  corpuses <- cens.getCorpusNames();
-  x <- .jnew(J("edu.cens.text.TermFreqOptionsDialog"), .jarray(corpuses));
-  x$setVisible(TRUE);
-
-
-
-  corpus <-  x$getCorpus();
-  percent <- x$getPercent();
-  sorted <- x$getSorted();
-  decreasing <- ! x$getAsc();
-
-  if(x$isOk())
-    return(cens.term_freq(get(corpus), percent, sorted, decreasing));
-}
-
-cens.choose_and_do <- function(f, ...) {
-  x <- cens.choose_corpus();
-  if(!is.null(x)) f(x, ...);
-}
-
-
 
 
