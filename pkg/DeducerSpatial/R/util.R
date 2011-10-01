9ab32d3bf80a6e18837d34919eded2991a7af9cb
ifellows <ifellows@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-01 08:18:33 +0000

    faster subsetting for polygon data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cens@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/DeducerSpatial/R/util.R
+++ pkg/DeducerSpatial/R/util.R
@@ -142,8 +142,6 @@ choro_plot <- function (sp, dem , cuts = list("quantile", seq(0,
 			bty = "o", title = legend.title, bg = "white")
 }
 
-#Used for rectangle subsetting
-#Returns true if *ALL* of the coords are contained by the rectangle
 .containedBy <- function (minLat, minLon, maxLat, maxLon, coords) {
 	minMerc <- project_mercator(minLat, minLon)
 	maxMerc <- project_mercator(maxLat, maxLon)
@@ -166,39 +164,39 @@ choro_plot <- function (sp, dem , cuts = list("quantile", seq(0,
 	}
 	return(TRUE)
 }
+
+#Used for rectangle subsetting
+#Returns true if *ALL* of the coords are contained by the rectangle
+.containedBy2 <- function (minMerc, maxMerc, coords) {
+	minLat <- minMerc[[1]]
+	maxLat <- maxMerc[[1]]
+	minLon <- minMerc[[2]]
+	maxLon <- maxMerc[[2]]
+	all(coords[,1] >= minLat & coords[,1] <= maxLat & coords[,2] >= minLon & coords[,2] <= maxLon)
+}
+
 #For Polygons
 #TODO handle error when no polys left.  Probably should do nothing.
 #TODO These following functions could likely be consolidated
 .subsetPoly <- function (minLat, minLon, maxLat, maxLon, polyDf, removeSelection) {
-	
+	minMerc <- project_mercator(minLat, minLon)
+	maxMerc <- project_mercator(maxLat, maxLon)
 	# The XOR inverts the function in any easy way w/o if/else statements
-	.contained <- function(poly){return(xor(!removeSelection, .containedBy(minLat, minLon, maxLat, maxLon, poly@coords)))}
+	.contained <- function(poly){return(xor(!removeSelection, .containedBy2(minMerc, maxMerc, poly)))}
 	
-	dupDf <- polyDf
+	nr <- nrow(polyDf)
+	indices <- rep(FALSE,nr)
 	
-	for (i in 1:length(polyDf@polygons)){# each list of polygons
-			dupDf@polygons[[i]]@Polygons <- Filter(.contained, polyDf@polygons[[i]]@Polygons)
-			#.containedBy(minLat, minLon, maxLat, maxLon, poly@coords)
+	for (i in 1:nr){
+			indices[i] <- .contained(t(polyDf[i,]@bbox)) 
 	}
 	
-	indices <- 1:length(polyDf)
-	indices <- Filter(function(x){return(length(dupDf@polygons[[x]]@Polygons) > 0)}, indices)
-	
-	if (length(indices) == 0)
-	{
+	if(all(!indices))
 		return(NULL)
-	}
-	else
-	{
-		dupDf <- polyDf[indices, ] #really, should be dupDf[indices, ] in case of multiple polys per entri
-		#dupDf@polygons <- Filter(function(x){return(length(x@Polygons) > 0)}, dupDf@polygons)
-		return(dupDf)
-	}
-	
+	polyDf[indices,]
 }
 
 .subsetLines <- function (minLat, minLon, maxLat, maxLon, polyDf, removeSelection) {
-	
 	# The XOR inverts the function in any easy way w/o if/else statements
 	.contained <- function(poly){return(xor(!removeSelection, .containedBy(minLat, minLon, maxLat, maxLon, poly@coords)))}
 	
